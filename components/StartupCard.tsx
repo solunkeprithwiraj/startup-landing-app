c65cc4071febd13f216cddc06f5de172b7b4c952
PrithwirajSolunke <prithwiraj.solunke@humancloud.co.in>
2025-03-31 23:57:19 +0530

    first commit

---
+++ components/StartupCard.tsx
@@ -0,0 +1,104 @@
+import { cn, formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Author, Startup } from "@/sanity/types";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
+
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  const {
+    _createdAt,
+    views,
+    author,
+    title,
+    category,
+    _id,
+    image,
+    description,
+  } = post;
+console.log("StartupCard post:", post);
+
+  return (
+    <li className="startup-card group">
+      <div className="flex-between">
+        <p className="startup_card_date">{formatDate(_createdAt)}</p>
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-primary" />
+          <span className="text-16-medium">{views ?? 0}</span>
+        </div>
+      </div>
+
+      <div className="flex-between mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${author?._id}`}>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+          </Link>
+        </div>
+
+        <Link href={`/user/${author?._id}`}>
+          <Image
+            src={
+              author?.image && author.image !== ""
+                ? author.image
+                : "/fallback-avatar.png"
+            }
+            alt={author?.name || "User Avatar"}
+            width={48}
+            height={48}
+            className="rounded-full"
+           
+          />
+        </Link>
+      </div>
+
+      <Link href={`/startup/${_id}`}>
+        <p className="startup-card_desc">
+          {description || "No Description available"}
+        </p>
+        {image && image !== "" ? (
+          <Image
+            src={image}
+            alt="Startup Image"
+            className="startup-card_img"
+            width={50}
+            height={50}
+          
+          />
+        ) : (
+          <Image
+            src="/user.png"
+            alt="Fallback Image"
+            className="startup-card_img"
+          />
+        )}
+      </Link>
+
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category?.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
+      </div>
+    </li>
+  );
+};
+
+export const StartupCardSkeleton = () => (
+  <>
+    {[0, 1, 2, 3, 4].map((index: number) => (
+      <li key={cn("skeleton", index)}>
+        <Skeleton className="startup-card_skeleton" />
+      </li>
+    ))}
+  </>
+);
+
+export default StartupCard;
---
+++ next.config.ts
@@ -0,0 +1,40 @@
+import { withSentryConfig } from "@sentry/nextjs";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // ✅ Allows images from ANY domain
+      },
+    ],
+    dangerouslyAllowSVG: true,
+  },
+  experimental: {
+    ppr: "incremental",
+    after: true,
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
+};
+
+export default withSentryConfig(nextConfig, {
+  org: "js-mastery-dk",
+  project: "javascript-nextjs",
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  reactComponentAnnotation: { enabled: true },
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
